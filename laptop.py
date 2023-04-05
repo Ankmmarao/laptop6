29214b347d31336bb98b4797dac66dd8326b7980
Ankmmarao <127002167+Ankmmarao@users.noreply.github.com>
2023-04-05 11:19:07 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -58,7 +58,8 @@ if st.checkbox("Get the corr of the dataset"):
     n=(data.corr())
     st.write(n)
 if st.checkbox("Get the Review Based on prices"):
-    df = pd.DataFrame(["price"],columns=["price", "no_of_rating"," no_of_reviews "])
+    df = pd.DataFrame([["price",50000,30000,],["price1",30000,45000]],columns=["price", "no_of_rating"," no_of_reviews "])
+
 
 fig = px.bar(df, x="price", y=["no_of_rating"," no_of_reviews "], barmode='group', height=400)
 # st.dataframe(df) # if need to display dataframe
